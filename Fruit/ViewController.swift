5a7f60c3da489771aa8b1d28739e11e97c2ec1c4
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-13 14:18:12 +0700

    Initial Commit

---
+++ Fruit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fruit
+//
+//  Created by arthithai.aamlid on 13/2/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
