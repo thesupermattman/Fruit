9a153d890e30cae16323a0ed2451be8a00459fe2
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-14 11:32:43 +0700

    Tableview done

---
+++ Fruit/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  Fruit
+//
+//  Created by arthithai.aamlid on 14/2/2566 BE.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+
+}
--- Fruit/ViewController.swift
+++ Fruit/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+        
+    @IBOutlet weak var table: UITableView!
+    
+    struct Fruit {
+        let title: String
+        let imageName: String
+    }
+    
+    let data: [Fruit] = [
+        Fruit(title: "Apple", imageName: "1"),
+        Fruit(title: "Orange", imageName: "2"),
+        Fruit(title: "Banana", imageName: "3"),
+        Fruit(title: "Grape", imageName: "4"),
+        Fruit(title: "Lemon", imageName: "5"),
+        Fruit(title: "Pear", imageName: "6")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let fruit = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.label.text = fruit.title
+        cell.iconImageView.image = UIImage(named: fruit.imageName)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
-
