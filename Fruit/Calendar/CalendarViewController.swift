8f38a6dec481949c5cf7e9fb494ea273352fe44c
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-20 18:18:48 +0700

    Changes made to size of cells

--- Fruit/Calendar/CalendarViewController.swift
+++ Fruit/Calendar/CalendarViewController.swift
@@ -24,13 +24,14 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
     
     @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
-            calendarViewModel.url = "https://www.set.or.th/api/set/holiday/year/2023?lang=th"
-            calendarViewModel.getData(url: calendarViewModel.url, callBack: getCalendarCallback)
+//            calendarViewModel.url = "https://www.set.or.th/api/set/holiday/year/2023?lang=th"
+            calendarViewModel.getData(url: "https://www.set.or.th/api/set/holiday/year/2023?lang=th", callBack: getCalendarCallback)
 
         } else if sender.selectedSegmentIndex == 1 { // If selectedSegementIdex == 1
-            calendarViewModel.url = "https://www.set.or.th/api/set/holiday/year/2023?lang=en"
-            calendarViewModel.getData(url: calendarViewModel.url, callBack: getCalendarCallback)
+//            calendarViewModel.url = "https://www.set.or.th/api/set/holiday/year/2023?lang=en"
+            calendarViewModel.getData(url: "https://www.set.or.th/api/set/holiday/year/2023?lang=en", callBack: getCalendarCallback)
         }
+//        calendarViewModel.getData(index: sender.selectedSegmentIndex, callBack: <#T##([CalendarViewModel.Response]?) -> Void#>)
     }
     
 
@@ -57,16 +58,30 @@ class CalendarViewController: UIViewController, UITableViewDataSource, UITableVi
         return cell
     }
     
-    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
+//    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
 }
 
 // To do
-// Fix tableview but set heightforrow at 0
-// Fix the date format dd DD MM yyyy
-// Fix the english/thai function
-// Try to understand callback more
-// Research compression priority and hugging content priority
-// Research Intrinsic content size
+// Fix tableview but set heightforrow at 0 (Partially complete)
+// Fix the date format dd DD MM yyyy (Completed)
+// Fix the english/thai function (Completed)
+
+// Research compression priority and hugging content priority (Completed)
+    //Hugging => content does not want to grow
+    //Compression Resistance => content does not want to shrink
+    // The content hugging priority determines how eager a view is to grow beyond its intrinsic content size.
+    // The content compression resistance priority determine how easy it is to compress the label if it needs to shrink.
+
+// Research Intrinsic content size (Completed)
+    // Intrinsic content size is when the UI object you're using knows what size it is. For example, you want the size of the a label to change depending on its contents, that is, the text it is displaying.
+
 // Research the different things in stackview such as fill, fill equally etc
+// Try to understand callback more
+
+// To Do
+// Fix didChangeSegment by putting a lot of it in the CalendarViewModel
+// Change the Thai date format to Thai language
+// Research what is deeplink
+// Read the rx files
