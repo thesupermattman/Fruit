3bec0246ac6fb2e492b46ed3af6fa77f14ead086
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-23 11:55:57 +0700

    Fruit changed to rx swift

--- Fruit/Main/MainViewController.swift
+++ Fruit/Main/MainViewController.swift
@@ -6,45 +6,77 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
     var viewModel = ViewModel()
+    private let bag = DisposeBag()
     var fruitWebView = FruitWebViewViewController()
+    var fruit: [ViewModel.Fruit] = []
             
     @IBOutlet weak var table: UITableView!
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        table.dataSource = self
+        table.rx.setDelegate(self).disposed(by: bag)
         table.delegate = self
+        table.dataSource = self
+        observeObservable()
+    }
+    
+    private func observeObservable() {
+//        table.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+//        viewModel.items.bind(to: table.rx.items(cellIdentifier: "cell", cellType: CustomTableViewCell.self)) { (row,item,cell) in
+//            cell.label?.text = title
+//            cell.iconImageView?.image = UIImage(named: viewModel.imageName)
+//        }.disposed(by: bag)
+//
+//        table.rx.modelSelected(Fruit.self).subscribe(onNext: { item in
+//            print()
+//        }).disposed(by: bag)
+//
+        viewModel.itemBehaviorSubject.subscribe(onNext: { fruitList in
+            self.fruit = fruitList
+            self.table.reloadData()
+        })
     }
 }
 
+//extension ViewController: UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 150
+//    }
+//
+//}
+
 extension ViewController: UITableViewDataSource {
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.getDataCount()
+        return fruit.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let fruit = viewModel.getData(for: indexPath)
+        let fruit = self.fruit[indexPath.row]
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
         cell.label.text = fruit.title
         cell.iconImageView.image = UIImage(named: fruit.imageName)
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 140
     }
 }
 
 extension ViewController: UITableViewDelegate {
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let url = viewModel.getFruitUrl(for: indexPath)
-                
+        let url = self.fruit[indexPath.row].url
+
         let storyBoard: UIStoryboard = UIStoryboard(name: "FruitWebView", bundle: nil)
         let newViewController = storyBoard.instantiateViewController(withIdentifier: "FruitWebView") as! FruitWebViewViewController
         newViewController.urlString = url
--- Fruit/Main/MainViewModel.swift
+++ Fruit/Main/MainViewModel.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class ViewModel {
     
-    private let data: [Fruit] = [
+    let fruit = [
         Fruit(title: "Apple", imageName: "1", url: "https://en.wikipedia.org/wiki/Apple"),
         Fruit(title: "Orange", imageName: "2", url: "https://en.wikipedia.org/wiki/Orange"),
         Fruit(title: "Banana", imageName: "3", url: "https://en.wikipedia.org/wiki/Banana"),
@@ -18,15 +20,34 @@ class ViewModel {
         Fruit(title: "Pear", imageName: "6", url: "https://en.wikipedia.org/wiki/Pear")
     ]
     
-    func getData(for indexPath: IndexPath) -> Fruit {
-        return data[indexPath.row]
-    }
+    var itemObservable = PublishSubject<[Fruit]>()
+    var itemBehaviorSubject = BehaviorSubject<[Fruit]>(value: [
+        Fruit(title: "Apple", imageName: "1", url: "https://en.wikipedia.org/wiki/Apple"),
+        Fruit(title: "Orange", imageName: "2", url: "https://en.wikipedia.org/wiki/Orange"),
+        Fruit(title: "Banana", imageName: "3", url: "https://en.wikipedia.org/wiki/Banana"),
+        Fruit(title: "Grape", imageName: "4", url: "https://en.wikipedia.org/wiki/Grape"),
+        Fruit(title: "Lemon", imageName: "5", url: "https://en.wikipedia.org/wiki/Lemon"),
+        Fruit(title: "Pear", imageName: "6", url: "https://en.wikipedia.org/wiki/Pear")
+    ])
     
-    func getDataCount() -> Int {
-        return data.count
+    func fetchItems() {
+        
+        itemObservable.onNext(fruit)
+        //itemObservable.onCompleted()
     }
-    
-    func getFruitUrl(for indexPath: IndexPath) -> String {
-        return data[indexPath.row].url
-    }
+//
+//        func getData(for indexPath: IndexPath) -> Fruit {
+//        return fetchItems()
+//    }
+//
+//    func getDataCount() -> Int {
+//        return data.count
+//    }
+//
+//    func getFruitUrl(for indexPath: IndexPath) -> String {
+//        return data[indexPath.row].url
+//    }
 }
+
+// Find out difference between publishSubject and behaviorSubject
+// Read about generic types for swift
